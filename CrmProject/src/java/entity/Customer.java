30f8f30517c39a0ac82023345babecce22a51aee
Baran Güçlü <gbaran180@gmail.com>
2024-04-27 21:57:04 +0300

    entity customer is update

--- CrmProject/src/java/entity/Customer.java
+++ CrmProject/src/java/entity/Customer.java
@@ -130,49 +130,4 @@ public class Customer implements BaseEntity{
     }
 
    
-
- 
-
-    
-
-
-public class Customer {
-    
-    Long id;
-    String name;
-    String surName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surName;
-    }
-
-    public void setSurname(String Surname) {
-        this.surName = Surname;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" + "id=" + id + ", name=" + name + ", Surname=" + surName + '}';
-    }
-
-    public Customer() {
-    }
-
-    
 }
