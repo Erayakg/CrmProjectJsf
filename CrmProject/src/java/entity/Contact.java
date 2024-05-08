3a07c251aa96133dab214af47844ae1b5a101630
Eray Akgül <90953626+Erayakg@users.noreply.github.com>
2024-05-08 19:16:48 +0300

    update entity

---
+++ CrmProject/src/java/entity/Contact.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author erayb
+ */
+public class Contact implements BaseEntity{
+
+    @Override
+    public long getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
